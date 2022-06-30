2bea2f1c0f05ac756534893fceb0d14b6a0d17b0
Guangbei <guangbei.yi@wsu.edu>
2022-06-29 22:19:56 -0700

    Update script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 collect.py -s collect
-python3 collect.py -s clone
-python3 collect.py -s readme
+nohup python3 collect.py -s collect > full.log 2>&1 &
+nohup python3 collect.py -s clone > full.log 2>&1 &
+nohup python3 collect.py -s readme > full.log 2>&1 &
